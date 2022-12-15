cd7d5b3fe95bf3c25cf2a6e48d7021671f037f42
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-15 23:56:49 +0530

    commit

--- projects/ex1.js
+++ projects/ex1.js
@@ -8,14 +8,28 @@ let div = document.querySelector("#div")
 let pro = document.querySelector("#pro")
 let output = document.querySelector("#output")
 
-function sumfunc(inputval1) {
-    // let sum = Number(inputval1.value) + Number(inputval2.value)
-    // output.innerText = sum
-    console.log(inputval1.value)
+function sumfunc() {
+    let sum = Number(inputval1.value) + Number(inputval2.value)
+    output.innerText = sum
+}
+
+function subfunc() {
+    let sub = Number(inputval1.value) - Number(inputval2.value)
+    output.innerText = sub
+}
+
+function divfunc() {
+    let div = Number(inputval1.value) / Number(inputval2.value)
+    output.innerText = div.toFixed(2)
+}
+
+function profunc() {
+    let pro = Number(inputval1.value) * Number(inputval2.value)
+    output.innerText = pro
 }
 
 sum.addEventListener("click", sumfunc)
-// sub.addEventListener("click", subfunc)
-// div.addEventListener("click", divfunc)
-// pro.addEventListener("click", profunc)
+sub.addEventListener("click", subfunc)
+div.addEventListener("click", divfunc)
+pro.addEventListener("click", profunc)
  
