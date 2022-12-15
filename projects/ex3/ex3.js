adea1f464a814aa920ef6c24a3434d2bd1b0648d
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-16 02:42:07 +0530

    commit

---
+++ projects/ex3/ex3.js
@@ -0,0 +1,41 @@
+let h1 = document.querySelector("#h1")
+let h2 = document.querySelector("#h2")
+let h3 = document.querySelector("#h3")
+let inputField = document.querySelector("#field")
+let outputView = document.querySelector(".outputfield")
+let reset = document.querySelector("#reset")
+
+
+
+function changeHead(event) {
+    let val = event.target.textContent
+    if (inputField.value) {
+        
+        if (val === "h1") {
+            outputView.innerHTML = `<h1>${inputField.value}</h1>`
+        }
+        else if (val == "h2") {
+            outputView.innerHTML = `<h2>${inputField.value}</h2>`
+        }
+        else {
+            outputView.innerHTML = `<h3>${inputField.value}</h3>`
+        }
+    }
+    else{
+        inputField.value = "enter valid string"
+    }
+}
+
+function resetFunc() {
+    outputView.innerHTML = ""
+    inputField.value = ""
+    let newElems = document.createElement("input")
+    newElems.setAttribute = ("id , field")
+    outputView.appendChild(newElems)
+}
+
+
+h1.addEventListener("click", changeHead)
+h2.addEventListener("click", changeHead)
+h3.addEventListener("click", changeHead)
+reset.addEventListener("click", resetFunc)
