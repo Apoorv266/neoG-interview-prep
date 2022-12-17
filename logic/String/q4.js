ae6a19138b1f37b3087d080256d1d09235313071
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-17 21:19:05 +0530

    commit

---
+++ logic/String/q4.js
@@ -0,0 +1,4 @@
+// Write a program that converts string like "124" to 124
+
+let str = "124"
+console.log(Number(str))
---
+++ logic/String/q5.js
@@ -0,0 +1,17 @@
+// Write a program to delete all vowels from a string. Assume string is not more than 80 characters long
+
+let vowel = ["a", "e", "i", "o", "u"]
+let str = "spongebob squarepants"
+let newStr = ''
+
+for (let i = 0; i < str.length; i++) {
+    let isVowel = false
+    for (let k = 0; k < vowel.length; k++) {
+        if (str[i] === vowel[k]){
+            isVowel = true
+        } 
+  }
+
+  if (!isVowel) newStr += str[i]
+}
+console.log(newStr)
