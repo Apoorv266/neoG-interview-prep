96bdfca1153ecace0f89d4e2ac333e566e096ffd
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-19 18:21:10 +0530

    q10 added

--- logic/String/q10.js
+++ logic/String/q10.js
@@ -1,13 +1,34 @@
 // Given a string, determine if it is a palindrome, considering only alphanumeric characters
 
-let str = "radar"
+let str = "level"
 
 // first method
 let str2 = str.toLowerCase()
 let str3 = (str.split("").reverse().join("").toLowerCase())
+
 if (str2 === str3) {
     console.log("true")
 }
-else{
+else {
     console.log("false")
 }
+
+//method second
+
+function checkPalim(string) {
+
+    let str = string.toLowerCase()
+    let start = 0
+    let end = string.length- 1
+
+    while (start < end) {
+        if (str[start] !== str[end]) {
+            return false
+        }
+        start++
+        end--
+    }
+    return true
+}
+
+console.log((checkPalim(str)))
