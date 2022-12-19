000d8c51fd7d185aac74c0e8b4d7661238937343
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-19 23:39:16 +0530

    commit

---
+++ logic/String/q11.js
@@ -0,0 +1,13 @@
+// For a given input string(str), write a function to print all the possible substrings.Without using substr method
+
+let testStr = "test"
+
+function extractFunc(str) {
+    for (let i = 0; i < str.length; i++) {
+        const element = array[i];
+        
+    }
+}
+
+console.log(extractFunc(str))
+
