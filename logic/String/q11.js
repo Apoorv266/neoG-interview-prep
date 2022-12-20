df07f748b93b3143193c236e7a946ff099c06967
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-20 23:52:02 +0530

    commit

--- logic/String/q11.js
+++ logic/String/q11.js
@@ -2,21 +2,42 @@
 
 let testStr = 'test'
 
+
+// with substring function 
 function extractFunc(str) {
     for (let i = 0; i < str.length; i++) {
-        for (var j = i + 1; j <= str.length; j++){
-
+        for (var j = i + 1; j <= str.length; j++) {
             // if (!arr.includes(str.substr(i, j))) {
             //     arr.push(str.substr(i, j))
             // }
             console.log(str.substring(i, j))
             // console.log(i, j)
         }
-
     }
 }
 extractFunc(testStr)
 
-console.log("-------------------------")
+console.log("------------------------")
+// without substring function
+
+function checkFunc(str) {
+    let arr = []
+    for (let i = 0; i < str.length; i++) {
+        arr.push(str[i])
+        for (let j = i + 1; j < str.length; j++) {
+
+            // appending to last item
+            console.log(i, j)
+            arr.push(
+                arr[arr.length - 1] + str[j]
+            );
+        }
+
+    }
+    return arr
+}
+
+console.log(checkFunc(testStr))
+
 
 
---
+++ logic/String/q12.js
@@ -0,0 +1,15 @@
+// Write a program that takes two strings and copies smaller string into bigger string
+
+let str1 = "This is string"
+let str2 = "This is test string"
+
+function copyFunc(str1, str2) {
+    if (str1.length < str2.length) {
+        let temp = str1
+        str1 = str2 
+        str2 = temp
+        console.log(str1, str2)
+    }
+}
+
+console.log(copyFunc(str1, str2))
