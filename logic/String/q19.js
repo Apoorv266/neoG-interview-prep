c20432037ec1b28d76d0183a05bc7605d59d2577
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-25 19:33:13 +0530

    added q19

--- logic/String/q19.js
+++ logic/String/q19.js
@@ -1,1 +1,19 @@
 // Write a program to toggle case of each character of the string "good afternoon" (example: "neogcamp" ⇒ "nEoGcAmP" )
+
+let str = "neogcamp"
+
+
+//method 1
+function toggleFunc(str) {
+    let newStr = ""
+    for (let i = 0; i < str.length; i++) {
+       if (i % 2 !== 0) {
+        newStr+=str[i].toUpperCase()
+       }else{
+        newStr+=str[i]
+       }
+    }
+    return newStr
+}
+
+console.log(toggleFunc(str))
