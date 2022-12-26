4562f7b42995317c4abeb3232cc86abf6911269c
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-26 14:35:27 +0530

    added q3, q5

--- logic/Array/q3.js
+++ logic/Array/q3.js
@@ -1,1 +1,20 @@
 // Find maximum and minimum of an array.
+let arr = [1, 2, 3, 4]
+
+function valFunc(arr) {
+    let max = 0
+    let min = Infinity
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+        if ( min > arr[i]) {
+            min = arr[i]
+        }
+        
+    }
+    console.log(max, min)
+}
+
+valFunc(arr)
---
+++ logic/Array/q5.js
@@ -0,0 +1,20 @@
+// Find sum of two arrays
+
+let arr1 = [3,5,2,9,4] 
+let arr2 = [6,2,8,1,3]
+
+function sumFunc(arr1, arr2) {
+let sum1 = 0
+let sum2 = 0
+
+for (let i = 0; i < arr1.length; i++) {
+   sum1 += arr1[i]
+}
+
+for (let j = 0; j < arr2.length; j++) {
+    sum2 += arr2[j]  
+}
+return sum1 + sum2
+}
+
+console.log(sumFunc(arr1, arr2))
