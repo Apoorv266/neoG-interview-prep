39bfd7325df9348cbeac3fd59afccb23b0630c40
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-27 20:35:54 +0530

    commit

--- mock interview question/ex5/index.js
+++ mock interview question/ex5/index.js
@@ -2,15 +2,18 @@ let url = "https://mystery-api.kushanksriraj.repl.co/get"
 
 function apiFunc(url) {
     fetch(url).then(response => {
-        if (!response.ok) {
-            if (resp.status == 404) {
-                throw new Error("page not found")
-            } else if (resp.status == 401) {
-                throw new Error(" You are not logged in")
-            }
+
+        if (resp.status == 404) {
+            throw new Error("page not found")
+        } else if (resp.status == 401) {
+            throw new Error(" You are not logged in")
         }
+        else if (resp.status == 200) {
+            throw new Error("API is working")
+        }
+
         return response.json()
-    }).then(response => console.log(response)).catch(error =>{
+    }).then(response => console.log(response)).catch(error => {
         console.log(error)
     })
 }
