e14f603c14cce3180d3db3a701ed286895aeffcf
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-29 00:18:04 +0530

    commit

--- more exercises/ex3/script.js
+++ more exercises/ex3/script.js
@@ -2,10 +2,20 @@
 
 let field = document.querySelector("#field")
 let btn = document.querySelector("#btn")
+let output = document.querySelector("#output")
 
 function checkFunc() {
+    let count = 0
+    let word
     let arr = field.value.split(" ")
-    console.log(arr)
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length > count) {
+            count = arr[i].length
+            word = arr[i]
+        }
+    }
+    let newStr = field.value.replace(word, `<span>${word}</span>`)
+    output.innerHTML = newStr
 }
 
 
