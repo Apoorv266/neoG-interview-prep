7d94c802b16798f2e784fd019517c40e2d3ad3f3
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-30 23:25:28 +0530

    commit

---
+++ logic/String/q22.js
@@ -0,0 +1,17 @@
+function containsSpecialChars(str) {
+  const specialChars = `\`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~`;
+
+  const result = specialChars.split('').some(specialChar => { 
+    if (str.includes(specialChar)) {
+      return true;
+    }
+
+
+    return false;
+  });
+
+  return result;
+}
+
+console.log(containsSpecialChars('hello')); // 👉️ false
+console.log(containsSpecialChars('hello!')); // 👉️ true
