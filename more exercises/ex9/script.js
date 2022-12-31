a7f36bf71e15b7356488e20d6ccc4b3baac2edfc
Apoorv Sharma <apoorvsharma494@gmail.com>
2022-12-31 17:11:04 +0530

    commit

--- more exercises/ex9/script.js
+++ more exercises/ex9/script.js
@@ -4,17 +4,51 @@
 let checkBox = document.querySelector("#age")
 let output = document.querySelector("#output")
 let name = document.querySelector("#name")
+let btn = document.querySelector("#btn")
+let timeout;
+let time = 0
 
 checkBox.addEventListener('input', function display() {
-   if (checkBox.checked && name.value !== "") {
-     output.innerText = `${name.value} is eligible`
-     output.style.color = "green"
-   }
-   else if(name.value !== ""){
-    output.innerText = `${name.value} is not eligible`
-    output.style.color = "red"
-   }else{ 
-    checkBox.checked = false
-    output.innerText = `Enter name first`
-   }
+
+    if (checkBox.checked && name.value !== "") {
+        output.innerText = `${name.value} is eligible`
+        output.style.color = "green"
+    }
+    else if (name.value !== "") {
+        output.innerText = `${name.value} is not eligible`
+        output.style.color = "red"
+    } else {
+        checkBox.checked = false
+        output.innerText = `Enter name first`
+    }
+
+
+
+
 })
+
+
+
+name.addEventListener("input", function input() {
+    output.innerText = ``
+    time = 1000
+    if (name.value == "") {
+        output.innerText = `Enter name first`
+        output.style.color = "Black"
+        checkBox.checked = false
+    }
+    setTimeout(call, time);
+})
+
+
+function call() {
+ if (!checkBox.checked && name.value !== "") {
+        output.innerText = `${name.value} is not eligible`
+        output.style.color = "red"
+    }
+    else if (checkBox.checked && name.value !== "") {
+        output.innerText = `${name.value} is eligible`
+        output.style.color = "green"
+    }
+}
+
